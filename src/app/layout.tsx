6dc177c0047d10caeb305da94b2f0c087e20633e
Akash Kadlag <akashkadlag14@gmail.com>
2024-08-01 03:17:07 +0530

    feat: add assets and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,5 +32,9 @@ export default function RootLayout({
 
 export const metadata: Metadata = {
   title: "Devs Overflow",
-  description: "A better version of Stack Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };

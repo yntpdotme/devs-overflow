bd76e4d6ac18531726280bdf0109e3e0d6f51ebb
Akash Kadlag <akashkadlag14@gmail.com>
2024-08-22 19:44:03 +0530

    feat: implement create operation for user routes

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,10 @@
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 
 import {User} from "@/database";
 import connectDB from "@/lib/db";
 import handleError from "@/lib/handlers/error";
+import {ValidationError} from "@/lib/http-errors";
+import {UserSchema} from "@/lib/schemas";
 import {APIErrorResponse} from "@/types";
 
 export const GET = async () => {
@@ -12,6 +14,29 @@ export const GET = async () => {
     const users = await User.find();
 
     return NextResponse.json({success: true, data: users}, {status: 200});
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+};
+
+export const POST = async (request: NextRequest) => {
+  try {
+    await connectDB();
+    const body = await request.json();
+
+    const {success, error, data} = UserSchema.safeParse(body);
+    if (!success) throw new ValidationError(error.flatten().fieldErrors);
+    const {email, username} = data;
+
+    const existingUser = await User.findOne({email});
+    if (existingUser) throw new Error("Email already in use");
+
+    const existingUsername = await User.findOne({username});
+    if (existingUsername) throw new Error("Username already taken");
+
+    const newUser = await User.create(data);
+
+    return NextResponse.json({success: true, data: newUser}, {status: 201});
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }

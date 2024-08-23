d611e230d75eb44135abf12a4d3bd4d60ae00c4a
Akash Kadlag <akashkadlag14@gmail.com>
2024-08-24 01:22:31 +0530

    feat: implement account lookup by provider

--- src/app/api/accounts/provider/route.ts
+++ src/app/api/accounts/provider/route.ts
@@ -1,24 +1,26 @@
-import {User} from "@/database";
+import {Account} from "@/database";
 import connectDB from "@/lib/db";
 import handleError from "@/lib/handlers/error";
 import {NotFoundError, ValidationError} from "@/lib/http-errors";
-import {UserSchema} from "@/lib/schemas";
+import {AccountSchema} from "@/lib/schemas";
 import {APIErrorResponse} from "@/types";
 import {NextRequest, NextResponse} from "next/server";
 
 export const POST = async (request: NextRequest) => {
-  const {email} = await request.json();
+  const {providerAccountId} = await request.json();
 
   try {
     await connectDB();
 
-    const {success, error, data} = UserSchema.partial().safeParse({email});
+    const {success, error, data} = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
     if (!success) throw new ValidationError(error.flatten().fieldErrors);
 
-    const user = await User.findOne(data);
-    if (!user) throw new NotFoundError("User");
+    const account = await Account.findOne(data);
+    if (!account) throw new NotFoundError("Account");
 
-    return NextResponse.json({success: true, data: user}, {status: 200});
+    return NextResponse.json({success: true, data: account}, {status: 200});
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
   }

5242f52a6ec8b5a6b5116a41f78e7d2cb946f53e
Akash Kadlag <akashkadlag14@gmail.com>
2024-08-25 17:12:57 +0530

    feat: implement fetch handler

---
+++ src/lib/handlers/fetch.ts
@@ -0,0 +1,54 @@
+import handleError from "@/lib/handlers/error";
+import {RequestError} from "@/lib/http-errors";
+import logger from "@/lib/logger";
+import {ActionResponse} from "@/types";
+
+type FetchOptions = RequestInit & {
+  timeout?: number;
+};
+
+const isError = (error: unknown): error is Error => error instanceof Error;
+
+export const fetchHandler = async <T>(
+  url: string,
+  options: FetchOptions = {}
+): Promise<ActionResponse<T>> => {
+  const {timeout = 5000, headers: customHeaders = {}, ...restOptions} = options;
+
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const defaultHeaders: HeadersInit = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
+  const headers: HeadersInit = {
+    ...defaultHeaders,
+    ...customHeaders,
+  };
+  const config: RequestInit = {
+    ...restOptions,
+    headers,
+    signal: controller.signal,
+  };
+
+  try {
+    const response = await fetch(url, config);
+
+    clearTimeout(id);
+
+    if (!response.ok) {
+      throw new RequestError(response.status, `HTTP error: ${response.status}`);
+    }
+
+    return response.json();
+  } catch (err) {
+    const error = isError(err) ? err : new Error("Unknown error");
+
+    if (error.name === "AbortError") logger.warn(`Request to ${url} timed out`);
+    else logger.error(`Error fetching ${url}: ${error.message}`);
+
+    return handleError(error) as ActionResponse<T>;
+  }
+};

c5d0b8c92e84b4f4ef840ba1cdb2581416b8b655
Akash Kadlag <akashkadlag14@gmail.com>
2024-10-21 23:16:56 +0530

    feat: show top questions

--- src/components/navigation/RightSidebar.tsx
+++ src/components/navigation/RightSidebar.tsx
@@ -1,15 +1,9 @@
+import DataRenderer from "@/components/DataRenderer";
 import ROUTES from "@/constants/routes";
+import {getHotQuestions} from "@/lib/actions";
 import Image from "next/image";
 import Link from "next/link";
 import TagCard from "../cards/TagCard";
-
-const hotQuestions = [
-  {_id: "1", title: "How to create a custom hook in React?"},
-  {_id: "2", title: "How to use React Query?"},
-  {_id: "3", title: "How to use Redux?"},
-  {_id: "4", title: "How to use React Router?"},
-  {_id: "5", title: "How to use React Context?"},
-];
 
 const popularTags = [
   {_id: "1", name: "react", questions: 100},
@@ -19,35 +13,48 @@ const popularTags = [
   {_id: "5", name: "react-query", questions: 75},
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const {success, data: hotQuestions, error} = await getHotQuestions();
+
   return (
     <section className="custom-scrollbar custom-scrollbar-left background-light900_dark200 light-border sticky left-0 top-0 flex h-full w-[300px] flex-col gap-1 overflow-y-auto border-r p-8 pb-6 pt-[80px] shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-semibold text-dark200_light900">Top Questions</h3>
 
-        <div className="mt-7 flex w-full flex-col gap-7">
-          {hotQuestions.map(({_id, title}, index) => (
-            <Link
-              key={_id}
-              href={ROUTES.QUESTION(_id)}
-              className="flex cursor-pointer items-start gap-2.5"
-            >
-              <Image
-                src={
-                  (index & 1) === 0
-                    ? "/icons/question-primary.svg"
-                    : "/icons/question-accent.svg"
-                }
-                alt="Question"
-                width={20}
-                height={20}
-              />
-              <p className="body-medium text-dark500_light700 self-end">
-                {title}
-              </p>
-            </Link>
-          ))}
-        </div>
+        <DataRenderer
+          success={success}
+          error={error}
+          data={hotQuestions}
+          empty={{
+            title: "No questions found",
+            message: "No questions have been asked yet.",
+          }}
+          render={hotQuestions => (
+            <div className="mt-7 flex w-full flex-col gap-7">
+              {hotQuestions.map(({_id, title}, index) => (
+                <Link
+                  key={_id}
+                  href={ROUTES.QUESTION(_id)}
+                  className="flex cursor-pointer items-start gap-2.5"
+                >
+                  <Image
+                    src={
+                      (index & 1) === 0
+                        ? "/icons/question-primary.svg"
+                        : "/icons/question-accent.svg"
+                    }
+                    alt="Question"
+                    width={20}
+                    height={20}
+                  />
+                  <p className="body-medium text-dark500_light700 self-end line-clamp-2">
+                    {title}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          )}
+        />
       </div>
 
       <div className="mt-12">

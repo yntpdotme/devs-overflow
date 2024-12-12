6a0f347023fe7a625d9052cc95b905f236c39619
Akash Kadlag <akashkadlag14@gmail.com>
2024-12-12 23:09:43 +0530

    feat: disable console logs on prod

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false
+  }
 };
 
 export default nextConfig;
